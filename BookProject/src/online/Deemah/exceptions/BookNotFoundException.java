4da9d46febe77cb349387a31d5df1f842681b178
Deemah Alsalamah <deemah-alsalamah38@hotmail.com>
2022-08-27 23:44:34 +0300

    Create BookNotFoundException.java

---
+++ BookProject/src/online/Deemah/exceptions/BookNotFoundException.java
@@ -0,0 +1,9 @@
+package online.Deemah.exceptions;
+
+public class BookNotFoundException extends Exception {
+
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+
+}
