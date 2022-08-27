177eb68ec130522a9a9eb28e5ac4f4c0a58dd8b7
Deemah Alsalamah <deemah-alsalamah38@hotmail.com>
2022-08-27 23:44:24 +0300

    Create ServiceException.java

---
+++ BookProject/src/online/Deemah/exceptions/ServiceException.java
@@ -0,0 +1,14 @@
+package online.Deemah.exceptions;
+
+
+    public class ServiceException extends Exception {
+
+        public ServiceException(String message) {
+            super(message);
+        }
+
+        public ServiceException(String message, Throwable cause) {
+            super(message, cause);
+        }
+    }
+
